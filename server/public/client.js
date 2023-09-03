77047375f6b14d6d510ae26cc14bfb917d0bff79
Najma Jama <Najma55.nj@gmail.com>
2023-09-03 13:56:36 -0500

    styles the table on css also created router folder

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,93 @@
-console.log( 'js' );
+console.log("hey js works");
+$(document).ready(onReady);
 
-$( document ).ready(onReady);{
-    console.log( 'JQ' );
+function onReady() {
+  console.log("jQuery works!!!");
+
+  getTask();
+  $("#addTask").on("click", postTask);
+  $("#taskTableBody").on('click', '.completeButton', markAsComplete);
+  $("#taskTableBody").on('click', '.deleteButton', deleteTask);
+
 }
- 
+
+function postTask() {
+  console.log("add task button");
+  let newTask = {
+    text: $("#taskInput").val(),
+  };
+  $.ajax({
+    method: "POST",
+    url: "/create-task",
+    data: newTask,
+  })
+    .then(function (response) {
+      $("input").val(""),
+        getTask();
+        console.log("task is created!");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+function getTask() {
+  $("#taskTableBody").empty();
+  $.ajax({
+    method: "GET",
+    url: "/get-tasks",
+  })
+    .then(function (response) {
+      // console.log("GET /task response", response);
+      for (let task of response) {
+        $("#taskTableBody").append(`
+          <tr class="" id=${task.id} data-id=${task.id}>
+          <td>${task.id}</td>
+          <td>${task.text}</td>
+            <td>${task.isComplete}</td>
+
+            <td><button  class="completeButton">COMPLETE</button></td>
+            <td><button class="deleteButton">DELETE</button></td>
+          
+          </tr>
+        
+        `);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+function markAsComplete() {
+    console.log('update');
+    let id = $(this).closest('tr').data('id');
+    
+
+    console.log('in updateTask', id);
+    $.ajax({
+        type: 'PUT',
+        url: `/mark-completed/${id}`
+    }).then(function (response) {
+        console.log('back from PUT:', response);
+        getTask();
+       
+    }).catch(function (err) {
+        console.log('error on client-side')
+    }) 
+}
+function deleteTask() {
+    console.log('delete');
+    let id = $(this).closest('tr').data('id');
+
+
+
+    console.log('in deleteTask', id);
+    $.ajax({
+        type: 'DELETE',
+        url: `/delete-task/${id}`
+    }).then(function (response) {
+        console.log('back from Delete:', response);
+        getTask();
+    }).catch(function (err) {
+        console.log('error on client-side')
+    }) 
+}
---
+++ server/routes/todo.router.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database/weekend-to-do-app');
+
+
+//Create a task -POST REQUEST- expect text - return task is completed 
+router.post('/create-task', (req, res) => {
+    console.log('hello world'); 
+    let newTask = req.body
+  
+    console.log("res.body:", req.body);
+  
+    const queryText = `
+    INSERT INTO tasks (text)
+    VALUES ($1)
+    `
+  
+    console.log('The query we\'re sending to postgres:', queryText);
+    pool.query(queryText, [newTask.text])
+        .then(
+            (result) => {
+                res.sendStatus(200);
+            }
+        ).catch(
+            (err) => {
+                console.log(`Error making query ${queryText}`, err);
+                res.sendStatus(500);
+            }
+        )
+  });
+  
+  // Mark as completed - patch request - Send ID - return updated task
+  router.put('/mark-completed/:taskId', (req, res) => {
+    console.log('hello world'); 
+    let taskId = req.params.taskId
+  console.log(taskId);
+   
+  
+    const queryText = `
+    UPDATE tasks SET "isComplete"='true' WHERE id = $1;
+    `
+  
+    console.log('The query we\'re sending to postgres:', queryText);
+  
+    pool.query(queryText, [taskId])
+        .then(
+            (result) => {
+                res.sendStatus(200);
+            }
+        ).catch(
+            (err) => {
+                console.log(`Error making query ${queryText}`, err);
+                res.sendStatus(500);
+            }
+        ) 
+  });
+  
+  // DELETE a task -  Delete request - Receive ID - message "task is deleted"
+  router.delete('/delete-task/:taskId', (req, res) => {
+    console.log('hello world');  
+    let taskId = req.params.taskId
+  
+   
+  
+    const queryText = `
+    DELETE FROM "tasks" WHERE "id" = $1;
+  
+    `
+  
+    console.log('The query we\'re sending to postgres:', queryText);
+  
+    // Then, find a way to pass it to our pool (aka, the connection to the db).
+    // Then it's pg's problem
+    pool.query(queryText, [taskId])
+        .then(
+            (result) => {
+                res.sendStatus(200);
+            }
+        ).catch(
+            (err) => {
+                console.log(`Error making query ${queryText}`, err);
+                res.sendStatus(500);
+            }
+        ) 
+  });
+  
+  
+  // Fetch All Task - Get request - no input - return all tasks
+  router.get('/get-tasks', (req, res) => {
+    console.log('hello world');  
+    let queryText = 'SELECT * FROM "tasks" ORDER BY id desc;'
+  
+    pool.query(queryText).then(
+        (result) => {
+            res.send(result.rows)
+        }
+    ).catch(
+        (err) => {
+            console.log(`Error making query ${queryText}`, err);
+            res.sendStatus(500);
+        }
+    )
+  });
+  module.exports = router;
